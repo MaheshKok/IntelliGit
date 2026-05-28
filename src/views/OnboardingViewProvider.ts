b2a8a3a2db9705d7489efe4c58d41cc2c6a03522
Mahesh Kokare <maheshkokare100@gmail.com>
2026-05-28 14:52:44 +0100

    fix image rendering

--- src/views/OnboardingViewProvider.ts
+++ src/views/OnboardingViewProvider.ts
@@ -86,7 +86,7 @@ export class OnboardingViewProvider implements vscode.WebviewViewProvider {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Security-Policy"
-        content="default-src 'none'; style-src 'nonce-${nonce}' ${webview.cspSource}; script-src 'nonce-${nonce}' ${webview.cspSource};">
+        content="default-src 'none'; style-src 'nonce-${nonce}' ${webview.cspSource}; script-src 'nonce-${nonce}' ${webview.cspSource}; img-src ${webview.cspSource} data:;">
     <title>${this.title}</title>
     <style nonce="${nonce}">
         * { box-sizing: border-box; margin: 0; padding: 0; }
